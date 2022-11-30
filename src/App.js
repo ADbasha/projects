66469c1be1f2c0b96efd9c4260178654d6ac7bcd
ADbasha <114794061+ADbasha@users.noreply.github.com>
2022-11-30 13:09:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+import CounterComponent from './components/CounterComponent'
+
+function App() {
+	const [count, setCount] = useState(0)
+	useEffect(() => {
+		console.log(count)
+	}, [count])
+
+  return (
+    <div className="App">
+			<CounterComponent count={count} setCount={setCount} />
+    </div>
+  );
+}
+
+export default App;
